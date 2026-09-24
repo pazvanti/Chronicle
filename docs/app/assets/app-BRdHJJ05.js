1fb15017c06a84d0bd3549b5b996e3254692106c
Petre Popescu <pazvanti2003@gmail.com>
2026-09-24 17:38:42 +0300

    Release version 1.5.0

--- docs/app/assets/app-BRdHJJ05.js
+++
@@ -1,1 +0,0 @@
-import{i as n}from"./index-D3VBi1Yy.js";async function o(){return n("plugin:app|version")}export{o as getVersion};
---
+++ docs/app/assets/app-wMhICWFc.js
@@ -0,0 +1,1 @@
+import{t as e}from"./index-CSsmu74H.js";async function t(){return e(`plugin:app|version`)}export{t as getVersion};
